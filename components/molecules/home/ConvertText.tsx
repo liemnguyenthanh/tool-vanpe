d41dfad5bafdf6bc40c8c8efa52f7f7f5ddf9594
liem_nguyen_thanh <liemdonduong@gmail.com>
2023-12-05 09:53:46 +0700

    chore: add text

--- components/molecules/home/ConvertText.tsx
+++ components/molecules/home/ConvertText.tsx
@@ -1,15 +1,38 @@
 import { useEditorContext } from '@/providers'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+
+const mappingHtmlToDictionary = (element: HTMLElement) => {
+  const tagName = element.tagName
+  console.log(tagName);
+  
+  return ''
+}
+
 
 export const ConvertText = () => {
   const { data } = useEditorContext()
+  const textRef = useRef<HTMLParagraphElement>(null)
+
+  useEffect(() => {
+    const convertData = () => {
+      if (textRef.current) {
+        const dataMap = mappingHtmlToDictionary(textRef.current)
+        console.log({ dataMap });
+
+      }
+    }
+    convertData()
+  }, [data])
+
+
+
   return (
-    <div className='rounded-lg border border-[#DAE0D580] bg-[#141a1f]'>
-      <p className="text-white font-bold p-4 border-b border-b-[#DAE0D580]">
+    <div className='rounded-lg border border-[#DAE0D580] bg-[#141a1f] overflow-hidden'>
+      <p className="text-white font-bold p-4 border-b border-b-[#DAE0D580] bg-[#1f262e]">
         Convert Text
       </p>
-      <div className="p-4 ">
-        <p className="text-white" dangerouslySetInnerHTML={{ __html: data }}></p>
+      <div className="p-4">
+        <p className="text-white" ref={textRef} dangerouslySetInnerHTML={{ __html: data }}></p>
       </div>
     </div>
   )
